195f50638bd6f91f583de34ae89a595467194017
Kristoffer Klevjer <kristoffer@klevjer.no>
2020-03-13 15:22:03 +0100

    Box-update

--- Data-cleaning-box.R
+++ Data-cleaning-box.R
@@ -7,8 +7,24 @@ library(openxlsx)
 path <- "/Users/klevjer/R Projects/Probabilistic Reasoning/Raw data/BoxTask/" #Set to local path / directory of raw files for Box-task
 pattern <- "ID_"
 BoxFiles <- list.files(path = path, pattern = pattern)
-BoxFiles <- paste(path, BoxFiles, sep = "")
-read.xlsx
+
+
+
+
+#Gjøres med alle
+test3 <- read.xlsx("/Users/klevjer/R Projects/Probabilistic Reasoning/Raw data/Safe to delete - Test files/ID_EN04NY19.xlsx", sheet=4)
+testlengde <- length(read.xlsx("/Users/klevjer/R Projects/Probabilistic Reasoning/Raw data/Safe to delete - Test files/ID_EN04NY19.xlsx", sheet=4)[,5]) #Returns draw number
+tidperbox <- mean((read.xlsx("/Users/klevjer/R Projects/Probabilistic Reasoning/Raw data/Safe to delete - Test files/ID_EN04NY19.xlsx", sheet=4)[1:testlengde,4]))
+vartidperbox <- var((read.xlsx("/Users/klevjer/R Projects/Probabilistic Reasoning/Raw data/Safe to delete - Test files/ID_EN04NY19.xlsx", sheet=4)[1:testlengde,4]))
+valg <- (read.xlsx("/Users/klevjer/R Projects/Probabilistic Reasoning/Raw data/Safe to delete - Test files/ID_EN04NY19.xlsx", sheet=4)[testlengde,5])
+probest <- valg <- (read.xlsx("/Users/klevjer/R Projects/Probabilistic Reasoning/Raw data/Safe to delete - Test files/ID_EN04NY19.xlsx", sheet=4)[testlengde,3])
+
+#Hente ut ID
+ID <- sapply(strsplit(BoxFiles, split='ID_*', fixed=TRUE), function(x) (x[1]))
+ID <- sapply(strsplit(ID, split='*.xlsx', fixed=TRUE), function(x) (x[1]))
+ID4 <- ID[4]
+ID4 <- sub(".*?ID_(.*?).xlsx.*", "\\1", ID4)
+
 
 #Trash
 path <- "/Users/klevjer/R Projects/Probabilistic Reasoning/Raw data/Ambiguity+Effort/" #Set to local path / directory of raw files for Ambiguity+Effort
