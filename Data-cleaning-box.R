5c8e077bcfda0c270ccb11f972d09ec7c5720862
Kristoffer Klevjer <kristoffer@klevjer.no>
2020-03-15 13:20:09 +0100

    Box-task cleaning complete

--- Data-cleaning-box.R
+++ Data-cleaning-box.R
@@ -2,9 +2,14 @@
 #Very important that the faw-files folder does not contain any other files, and, that you don't have any of them opened
 #as this will create a temp-file in that folders that inhibits this script-file from running properly.
 
-#Get the openxlsx package to read Excel-documents
-#install.packages("openxlsx", dependencies = TRUE) #remove comment in front first time
-library(openxlsx)
+#Get the openxlsx package to read Excel-documents (if not installed), and load the package
+pakke <- library()
+if (sum(grepl("openxlsx", pakke$results)) < 1) {
+  install.packages("openxlsx", dependencies = TRUE)
+  library(openxlsx)
+} else {
+  library(openxlsx)
+}
 
 path <- "/Users/klevjer/R Projects/Probabilistic Reasoning/Raw data/BoxTask/" #Set to local path / directory of raw files for Box-task
 pattern <- "ID_"
@@ -24,6 +29,7 @@ BoxTask10 <- BoxTask
 BoxTask11 <- BoxTask
 BoxTask12 <- BoxTask
 
+#Extract from xlsx-files
 for (i in BoxFiles) {
   print(i)
   pathspes <- paste("/Users/klevjer/R Projects/Probabilistic Reasoning/Raw data/BoxTask/",i, sep="")
@@ -118,3 +124,22 @@ for (i in BoxFiles) {
   Run <- cbind(ID, lengde, tidperbox, vartidperbox, valg, probest)
   BoxTask12 <- rbind(BoxTask12,Run)
 }
+
+#Combine all sheets/info, set ID as rownames and remove redundencies
+BoxTaskAll <- cbind(BoxTask1, BoxTask2, BoxTask3, BoxTask4, BoxTask6, BoxTask7, BoxTask8, BoxTask10, BoxTask11, BoxTask12)
+BoxTaskIDs <- BoxTaskAll[[1]]
+BoxTaskAll <- BoxTaskAll[-c(1,7,13,19,25,31,37,43,49,55,61)]
+row.names(BoxTaskAll) <- BoxTaskIDs
+
+#Rearrange the results, and add headers
+Colnavn <- c("DtDN1","DtDN2","DtDN3","DtDN4","DtDN5","DtDN6","DtDN7","DtDN8","DtDN9","DtDN10","BoxMeanRT1","BoxMeanRT2","BoxMeanRT3","BoxMeanRT4","BoxMeanRT5","BoxMeanRT6","BoxMeanRT7","BoxMeanRT8","BoxMeanRT9","BoxMeanRT10","BoxVarRT1","BoxVarRT2","BoxVarRT3", "BoxVarRT4", "BoxVarRT5","BoxVarRT6","BoxVarRT7","BoxVarRT8","BoxVarRT9","BoxVarRT10","BoxChoice1","BoxChoice2","BoxChoice3","BoxChoice4","BoxChoice5","BoxChoice6","BoxChoice7","BoxChoice8","BoxChoice9","BoxChoice10","BoxLastProb1","BoxLastProb2","BoxLastProb3","BoxLastProb4","BoxLastProb5","BoxLastProb6","BoxLastProb7","BoxLastProb8","BoxLastProb9","BoxLastProb10")
+BoxTaskLengde <- BoxTaskAll[c(1,6,11,16,21,26,31,36,41,46)]
+BoxTaskTid <- BoxTaskAll[c(2,7,12,17,22,27,32,37,42,47)]
+BoxTaskVar <- BoxTaskAll[c(3,8,13,18,23,28,33,38,43,48)]
+BoxTaskValg <- BoxTaskAll[c(4,9,14,19,24,29,34,39,44,49)]
+BoxTaskProb <- BoxTaskAll[c(5,10,15,20,25,30,35,40,45,50)]
+BoxTaskFerdig <- cbind(BoxTaskLengde,BoxTaskTid,BoxTaskVar,BoxTaskValg,BoxTaskProb)
+colnames(BoxTaskFerdig) <- Colnavn
+
+#Create CSV-file
+write.csv(BoxTaskFerdig, "BoxTask.csv")
