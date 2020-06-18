fe11534f8142fd866b1d8cfde694f774fcc59060
Kristoffer Klevjer <kristoffer@klevjer.no>
2020-06-18 16:15:24 +0200

    FINISHED - All info extracted, quality control done

--- Data-extraction-BeadsOnejarTask.R
+++ Data-extraction-BeadsOnejarTask.R
@@ -1,32 +1,55 @@
 #Data extraction BeadsOnejarTask
-## Yet to do: Fix participant ID
 
 path <- BeadsOnejarTaskPath
 pattern <- "trials_2.csv"
 DtDPath <- path[grepl(pattern = pattern, path)]
 DtDN <- do.call(rbind, lapply(DtDPath, function(x) sum(as.numeric(as.character(read.csv(x)[1:20,2])))))
+RTMean <- do.call(rbind, lapply(DtDPath, function(x) mean(as.numeric(as.character(read.csv(x)[2:20,4])), na.rm = TRUE)))
+RTMedian <- do.call(rbind, lapply(DtDPath, function(x) median(as.numeric(as.character(read.csv(x)[2:20,4])), na.rm = TRUE)))
+RTsd <- do.call(rbind, lapply(DtDPath, function(x) sd(as.numeric(as.character(read.csv(x)[2:20,4])), na.rm = TRUE)))
 DtDName <- do.call(rbind, lapply(DtDPath, function(x) as.character((read.csv(x)[,1:2])[(read.csv(x)[,1:2])=="participant",2])))
+DtDName <- paste("X", DtDName, sep="")
+DtDName <- sub("_.*", "", DtDName)
+DtDN <- cbind(DtDN, RTMean, RTMedian, RTsd)
 row.names(DtDN) <- DtDName
 DtDN2 <- DtDN
 
 pattern <- "trials_3.csv"
 DtDPath <- path[grepl(pattern = pattern, path)]
 DtDN <- do.call(rbind, lapply(DtDPath, function(x) sum(as.numeric(as.character(read.csv(x)[1:20,2])))))
+RTMean <- do.call(rbind, lapply(DtDPath, function(x) mean(as.numeric(as.character(read.csv(x)[2:20,4])), na.rm = TRUE)))
+RTMedian <- do.call(rbind, lapply(DtDPath, function(x) median(as.numeric(as.character(read.csv(x)[2:20,4])), na.rm = TRUE)))
+RTsd <- do.call(rbind, lapply(DtDPath, function(x) sd(as.numeric(as.character(read.csv(x)[2:20,4])), na.rm = TRUE)))
 DtDName <- do.call(rbind, lapply(DtDPath, function(x) as.character((read.csv(x)[,1:2])[(read.csv(x)[,1:2])=="participant",2])))
+DtDName <- paste("X", DtDName, sep="")
+DtDName <- sub("_.*", "", DtDName)
+DtDN <- cbind(DtDN, RTMean, RTMedian, RTsd)
 row.names(DtDN) <- DtDName
 DtDN3 <- DtDN
 
 pattern <- "trials_4.csv"
 DtDPath <- path[grepl(pattern = pattern, path)]
 DtDN <- do.call(rbind, lapply(DtDPath, function(x) sum(as.numeric(as.character(read.csv(x)[1:20,2])))))
+RTMean <- do.call(rbind, lapply(DtDPath, function(x) mean(as.numeric(as.character(read.csv(x)[2:20,4])), na.rm = TRUE)))
+RTMedian <- do.call(rbind, lapply(DtDPath, function(x) median(as.numeric(as.character(read.csv(x)[2:20,4])), na.rm = TRUE)))
+RTsd <- do.call(rbind, lapply(DtDPath, function(x) sd(as.numeric(as.character(read.csv(x)[2:20,4])), na.rm = TRUE)))
 DtDName <- do.call(rbind, lapply(DtDPath, function(x) as.character((read.csv(x)[,1:2])[(read.csv(x)[,1:2])=="participant",2])))
+DtDName <- paste("X", DtDName, sep="")
+DtDName <- sub("_.*", "", DtDName)
+DtDN <- cbind(DtDN, RTMean, RTMedian, RTsd)
 row.names(DtDN) <- DtDName
 DtDN4 <- DtDN
 
 pattern <- "trials.csv"
 DtDPath <- path[grepl(pattern = pattern, path)]
 DtDN <- do.call(rbind, lapply(DtDPath, function(x) sum(as.numeric(as.character(read.csv(x)[1:20,2])))))
+RTMean <- do.call(rbind, lapply(DtDPath, function(x) mean(as.numeric(as.character(read.csv(x)[2:20,4])), na.rm = TRUE)))
+RTMedian <- do.call(rbind, lapply(DtDPath, function(x) median(as.numeric(as.character(read.csv(x)[2:20,4])), na.rm = TRUE)))
+RTsd <- do.call(rbind, lapply(DtDPath, function(x) sd(as.numeric(as.character(read.csv(x)[2:20,4])), na.rm = TRUE)))
 DtDName <- do.call(rbind, lapply(DtDPath, function(x) as.character((read.csv(x)[,1:2])[(read.csv(x)[,1:2])=="participant",2])))
+DtDName <- paste("X", DtDName, sep="")
+DtDName <- sub("_.*", "", DtDName)
+DtDN <- cbind(DtDN, RTMean, RTMedian, RTsd)
 row.names(DtDN) <- DtDName
 DtDN1 <- DtDN
 
@@ -35,10 +58,10 @@ BeadsOnejarTaskDtD <- merge(BeadsOnejarTaskDtD, DtDN3, by.x = 1, by.y = 0)
 BeadsOnejarTaskDtD <- merge(BeadsOnejarTaskDtD, DtDN4, by.x = 1, by.y = 0)
 
 row.names(BeadsOnejarTaskDtD) <- BeadsOnejarTaskDtD[,1]
-BeadsOnejarTaskDtD <- BeadsOnejarTaskDtD[,-1]
+BeadsOnejarTask <- BeadsOnejarTaskDtD[,-1]
 
-rm(DtDN, DtDN1, DtDN2, DtDN3, DtDN4, DtDName, DtDPath, path, pattern)
+rm(DtDN, DtDN1, DtDN2, DtDN3, DtDN4, DtDName, DtDPath, path, pattern, BeadsOnejarTaskDtD, RTMean, RTMedian, RTsd)
 
-colnames(BeadsOnejarTaskDtD) <- c("Trial1", "Trial2", "Trial3", "Trial4")
+colnames(BeadsOnejarTask) <- c("BeadsOnejarDtD1", "BeadsOnejarRTmean1", "BeadsOnejarRTmedian1", "BeadsOnejarRTsd1", "BeadsOnejarDtD2", "BeadsOnejarRTmean2", "BeadsOnejarRTmedian2", "BeadsOnejarRTsd2", "BeadsOnejarDtD3", "BeadsOnejarRTmean3", "BeadsOnejarRTmedian3", "BeadsOnejarRTsd3", "BeadsOnejarDtD4", "BeadsOnejarRTmean4", "BeadsOnejarRTmedian4", "BeadsOnejarRTsd4")
 
-write.csv(BeadsOnejarTaskDtD, paste(getwd(), "/Cleaned data/BeadsOnejarTaskDtD.csv", sep = ""))
+write.csv(BeadsOnejarTask, paste(getwd(), "/Cleaned data/BeadsOnejarTask.csv", sep = ""))
