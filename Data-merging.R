2449a0a05b2dc656dcb90b083633b1869bf77751
Kristoffer Klevjer <kristoffer@klevjer.no>
2020-06-29 04:03:34 +0200

    Merges all data

--- Data-merging.R
+++ Data-merging.R
@@ -1,2 +1,32 @@
 # Merging from env
 ## Script for merging after running the data-extraction scripts (i.e., not by using the CSV-files)
+
+All <- merge(ARTEst, BeadsOnejarTask, by = "row.names", all.x = TRUE, all.y = TRUE)
+row.names(All) <- All[,1]
+All <- All[,-1]
+
+All <- merge(All, BeadsToneTask, by = "row.names", all.x = TRUE, all.y = TRUE)
+row.names(All) <- All[,1]
+All <- All[,-1]
+
+All <- merge(All, BeadsTwojarTask, by = "row.names", all.x = TRUE, all.y = TRUE)
+row.names(All) <- All[,1]
+All <- All[,-1]
+
+All <- merge(All, BoxIrregTask, by = "row.names", all.x = TRUE, all.y = TRUE)
+row.names(All) <- All[,1]
+All <- All[,-1]
+
+All <- merge(All, BoxNormTaskLog, by = "row.names", all.x = TRUE, all.y = TRUE)
+row.names(All) <- All[,1]
+All <- All[,-1]
+
+All <- merge(All, BoxNormTaskXlsx, by = "row.names", all.x = TRUE, all.y = TRUE)
+row.names(All) <- All[,1]
+All <- All[,-1]
+
+All <- merge(All, Cape, by = "row.names", all.x = TRUE, all.y = TRUE)
+row.names(All) <- All[,1]
+All <- All[,-1]
+
+write.csv(All, file = paste(getwd(), "/Cleaned data/AllData.csv", sep =""))
