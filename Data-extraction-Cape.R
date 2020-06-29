5dc4d4d9e22a1a814651fd8ace8907b1eb9f23d2
Kristoffer Klevjer <kristoffer@klevjer.no>
2020-06-29 02:32:34 +0200

    Cape-extraction and diviation into capep and control done

--- Data-extraction-Cape.R
+++ Data-extraction-Cape.R
@@ -1,25 +1,53 @@
-#Data extraction CAPE-P (includes the three control-questions)
+#Data extraction CAPE-P
 
 Path <- CapePath[grepl("CAPE_p.csv", CapePath)]
 Cape2018 <- row.names(c("ID", "Score"))
 Cape2018 <- as.data.frame(Cape2018)
 
 for (i in Path) {
-test <- read.csv(i)
-id <- (as.character(test[51,2]))
-capp <- c(2,5, 6,7,10,12,14,16,18,21,22,33,24,26,28,30,32,34,36,37,45,44,46)
-test <- test[capp,]
-totalt <- as.numeric(colSums(test[4]))
-Cape2018 <- rbind(Cape2018,cbind(id, totalt))
+score <- read.csv(i)
+id <- (as.character(score[51,2]))
+capp <- c(2,5,6,7,10,12,14,16,18,21,24,26,28,30,32,34,36,37,45,46)
+control <- c(22,33,44)
+capp <- score[capp,]
+control <- score[control,]
+capp <- as.numeric(colSums(capp[4]))
+control <- as.numeric(colSums(control[4]))
+Cape2018 <- rbind(Cape2018,cbind(id, capp, control))
 }
 
-cape2020 <- cbind(music[1], rowSums(music[2:24]))
-colnames(Cape2018) <- c("ID", "Score")
-colnames(cape2020) <- c("ID", "Score")
-Cape2018$ID <- as.character(Cape2018$ID)
-Cape2018$Score <- as.numeric(Cape2018$Score)
-cape2020$ID <- as.character(cape2020$ID)
-cape2020$Score <- as.numeric(cape2020$Score)
-CapeScore <- rbind(Cape2018, cape2020) 
+Cape2020 <- read.csv(paste(getwd(), "/Intermidiate data/music_data.csv", sep = ""))
+control <- c(5,12,22)
+capp <- c(1,2,3,4,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,23)
+control <- control + 13
+capp <- capp + 13
+id <- as.character(Cape2020[,2])
+capp <- as.numeric(rowSums(Cape2020[,capp]))
+control <- as.numeric(rowSums(Cape2020[,control]))
+Cape2020 <- cbind(id, capp, control)
 
-write.csv(CapeScore, paste(getwd(), "/Cleaned data/CAPE.csv", sep=""))
+#Manual fix of participant-numbers
+Cape2020 <- Cape2020[-1,]
+Cape2018[,1] <- as.character(Cape2018[,1])
+Cape2018[12,1] <- 312
+Cape2018[14,1] <- 314
+Cape2018[16,1] <- 316
+Cape2018[18,1] <- 318
+Cape2018[19,1] <- 319
+Cape2018[20,1] <- 320
+Cape2018[21,1] <- 321
+Cape2018[22,1] <- 322
+Cape2018[24,1] <- 324
+Cape2018[25,1] <- 325
+Cape2018[27:35,1] <- as.character(paste("00", seq(from=1, to=9), sep =""))
+Cape2018[36:48,1] <- as.character(paste("0", seq(from=10, to=22), sep =""))
+Cape2018[,1] <- paste("X", Cape2018[,1], sep ="")
+
+Cape <- rbind(Cape2020, Cape2018)
+row.names(Cape) <- Cape[,1]
+Cape <- Cape[,-1]
+colnames(Cape) <- c("CapeP", "CapeControl")
+
+write.csv(Cape, paste(getwd(), "/Cleaned data/CAPE.csv", sep=""))
+
+rm(Cape2018, Cape2020, score, CapePath, capp, control, i, id, Path)
