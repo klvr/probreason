e9ca711981c58e2a786a00bdbd5857ad4e364e18
Kristoffer Klevjer <kristoffer@klevjer.no>
2020-06-29 03:04:24 +0200

    Added master-script for running all data-extraction, cleaning and merging scripts.

---
+++ MasterScript-DataExtractionMerging.R
@@ -0,0 +1,13 @@
+# Master-script Data extraction and merging
+## NB: If hBayesDM-data does not exist, it will run these two! Depending on your system this will take 12hours!
+source("Loading-files.R")
+source("Data-extraction-ART.R")
+source("Data-extraction-BeadsOnejarTask.R")
+source("Data-extraction-BeadsToneTask.R")
+source("Data-extraction-BeadsTwojarTask.R")
+source("Data-extraction-BoxIrregTask.R")
+source("Data-extraction-BoxNormTaskLog.R")
+source("Data-extraction-BoxNormTaskXlsx.R")
+source("Data-extraction-Cape.R")
+source("hBayesDM-ART.R")
+source("Data-merging.R")
