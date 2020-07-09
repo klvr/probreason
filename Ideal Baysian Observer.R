c31cebeee1af5985aada88ae58c06f06ab13ab74
Kristoffer Klevjer <kristoffer@klevjer.no>
2020-07-09 15:36:25 +0200

    BF's for the beads and no-ratio Box

--- Ideal Baysian Observer.R
+++ Ideal Baysian Observer.R
@@ -1,8 +1,5 @@
 # Ideale Baysian Observer
 
-# Step 1: Without prior/other information, i.e., purely "majority".
-# Step 2: Integrate prior/other information
-
 # 2018
 ## One Jar
 ### Seq1_8515, Seq3_8515, Seq1_6040, Seq3_6040
@@ -27,37 +24,73 @@ Seq3_8515 <- c(1, 0, rep(1, 5), 0, rep(1, 3), 0, rep(1, 5), 0, 1, 1)
 Seq1_6040 <- c(1, 1, 0, 0, 1, 0, 0, rep(1, 5), 0, 1, 0, 1, 1, 1, 0, 1)
 Seq3_6040 <- c(0, rep(1, 3), 0, 1, 0, 1, 1, 0, 0, 1, 0, rep(1, 4), 0, 1, 0)
 Seq4_8515 <- c(rep(1,7),0,rep(1,12))
+AllBeadsSeq <- c("Seq1_8515", "Seq2_8515", "Seq3_8515","Seq4_8515", "Seq1_6040", "Seq3_6040")
 
 # Actual sequences Box task (2020 fixed)
-seq_12_1 <- c(1,1,1,0,1,1,1,1,0,1,0,1) #0: Blue, 1: Red
-seq_12_2 <- c(0,1,0,1,0,0,1,0,1,0,1,0) #0: Yellow, 1: Green
-seq_12_3 <- c(0,0,1,0,1,0,1,0,0,1,0,0) #0: Purple, 1: White
-seq_6or9a_1 <- c(0,0,0,1,0,0,0,0,1) #0: Light blue, 1: Red
-seq_6or9a_2 <- c(1,0,0,1,0,0) #0: Yellow, 1: Light Green
-seq_6or9a_3 <- c(1,0,0,0,1,0,1,0,0) #0: Light purple, 1: Yellowish white
-seq_6or9b_1 <- c(0,1,0,1,0,1,0,1,0) #0: Red, 1: Green
-seq_6or9b_2 <- c(0,0,1,0,1,0) #0: Pink, 1: Green
-seq_6or9b_3 <- c(0,0,0,1,0,0) #0: Blue, 1: Light Yellow
+Seq_12_1 <- c(1,1,1,0,1,1,1,1,0,1,0,1) #0: Blue, 1: Red
+Seq_12_2 <- c(0,1,0,1,0,0,1,0,1,0,1,0) #0: Yellow, 1: Green
+Seq_12_3 <- c(0,0,1,0,1,0,1,0,0,1,0,0) #0: Purple, 1: White
+Seq_6or9a_1 <- c(0,0,0,1,0,0,0,0,1) #0: Light blue, 1: Red
+Seq_6or9a_2 <- c(1,0,0,1,0,0) #0: Yellow, 1: Light Green
+Seq_6or9a_3 <- c(1,0,0,0,1,0,1,0,0) #0: Light purple, 1: Yellowish white
+Seq_6or9b_1 <- c(0,1,0,1,0,1,0,1,0) #0: Red, 1: Green
+Seq_6or9b_2 <- c(0,0,1,0,1,0) #0: Pink, 1: Green
+Seq_6or9b_3 <- c(0,0,0,1,0,0) #0: Blue, 1: Light Yellow
+AllBoxSeqUnlimit <- c("Seq_12_1","Seq_12_2","Seq_12_3")
+AllBoxSeqLimit6 <- c("Seq_6or9a_2","Seq_6or9b_2","Seq_6or9b_3")
+AllBoxSeqLimit9  <- c("Seq_6or9a_1","Seq_6or9a_3","Seq_6or9b_1")
 
-# Test 1a
-seq <- seq_12_1
-x <- vector(mode = "numeric")
-for (i in seq) {
-p_grid <- seq(0,1,length.out = 1000)
-prior <- rep(1, 1000)
-x <- c(x, i)
-likelihood <- dbinom(sum(x), size = length(x), prob = p_grid)
-unstd.posterior <- likelihood*prior
-posterior <- unstd.posterior/sum(unstd.posterior)
-plot(p_grid, posterior, type="b")
-samples <- sample(p_grid, size=1e4, replace=TRUE, prob=posterior)
-print(mean(samples < 0.5))
+# Beads
+ThresholdBeads <- as.numeric()
+for (i in AllBeadsSeq) {
+seq <- eval(parse(text = i))
+seq <- cumsum(seq)
+lengde <- seq(1, length(seq), by = 1)
+low <- (as.numeric(unlist(strsplit(i, "_[0-9][0-9]"))[2]) / 100)
+high <- 1-low
+Threshold <- dbinom(seq, lengde, prob = high) / dbinom(seq, lengde, prob = low)
+ThresholdBeads <- cbind(ThresholdBeads, Threshold)
 }
+colnames(ThresholdBeads) <- AllBeadsSeq
 
-# Test 1b
-samples <- sample(p_grid, prob=posterior, size=1e4, replace=TRUE)
-plot(samples)
-library(rethinking)
-dens(samples)
-HPDI(samples, prob=0.5)
+# Box
+ThresholdBox12 <- as.numeric()
+for (i in AllBoxSeqUnlimit) {
+seq <- eval(parse(text = i))
+if(sum(seq)/length(seq) < .5) {seq <- ((seq*-1)+1)}
+probl <- 6
+probh <- 6
+problow <- probl - cumsum(seq)
+probhigh <- probh - cumsum((seq*-1)+1)
+ThresholdSeq <- probhigh / (probhigh+problow)
+ThresholdSeq <- replace(ThresholdSeq, ThresholdSeq>1, 1)
+ThresholdBox12 <- cbind(ThresholdBox12, ThresholdSeq)
+}
+colnames(ThresholdBox12) <- AllBoxSeqUnlimit
+ThresholdBox9 <- as.numeric()
+for (i in AllBoxSeqLimit9) {
+seq <- eval(parse(text = i))
+if(sum(seq)/length(seq) < .5) {seq <- ((seq*-1)+1)}
+probl <- 6
+probh <- 6
+problow <- probl - cumsum(seq)
+probhigh <- probh - cumsum((seq*-1)+1)
+ThresholdSeq <- probhigh / (probhigh+problow)
+ThresholdSeq <- replace(ThresholdSeq, ThresholdSeq>1, 1)
+ThresholdBox9 <- cbind(ThresholdBox9, ThresholdSeq)
+}
+colnames(ThresholdBox9) <- AllBoxSeqLimit9
+ThresholdBox6 <- as.numeric()
+for (i in AllBoxSeqLimit6) {
+seq <- eval(parse(text = i))
+if(sum(seq)/length(seq) < .5) {seq <- ((seq*-1)+1)}
+probl <- 6
+probh <- 6
+problow <- probl - cumsum(seq)
+probhigh <- probh - cumsum((seq*-1)+1)
+ThresholdSeq <- probhigh / (probhigh+problow)
+ThresholdSeq <- replace(ThresholdSeq, ThresholdSeq>1, 1)
+ThresholdBox6 <- cbind(ThresholdBox6, ThresholdSeq)
+}
+colnames(ThresholdBox6) <- AllBoxSeqLimit6
 
