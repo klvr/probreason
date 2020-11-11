949e2564821a25ee4a1af5981cb207f87ec3e630
Kristoffer Klevjer <kristoffer@klevjer.no>
2020-11-11 15:58:52 +0100

    Added manual-fix script

--- MasterScript-DataExtractionMerging.R
+++ MasterScript-DataExtractionMerging.R
@@ -1,5 +1,5 @@
 # Master-script Data extraction and merging
-## NB: If hBayesDM-data does not exist, it will run these two! Depending on your system this will take 12hours!
+## NB: If hBayesDM-data does not exist, it will run these too! Depending on your system this will take 12hours!
 source("Loading-files.R")
 source("Data-extraction-ART.R")
 source("Data-extraction-BeadsOnejarTask.R")
@@ -10,4 +10,5 @@ source("Data-extraction-BoxNormTaskLog.R")
 source("Data-extraction-BoxNormTaskXlsx.R")
 source("Data-extraction-Cape.R")
 source("hBayesDM-ART.R")
+source("Manual-ID-fix.R")
 source("Data-merging.R")
