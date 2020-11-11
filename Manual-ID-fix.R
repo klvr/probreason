e0c9e37e691db556c70d04714ac8c71e5d0621f0
Kristoffer Klevjer <kristoffer@klevjer.no>
2020-11-11 16:07:30 +0100

    Script for manual fixes of ID's & removals

---
+++ Manual-ID-fix.R
@@ -0,0 +1,27 @@
+# Manual fixing of ID's and removals in BoxBeadsART 2020 participants.
+## Based on missing data, missing ID's and computer ID and timestamps. Err on the side of losing data rather than "overmatching".
+
+# Spelling errors in ID.
+## HL07EN21 swapped HL in BeadsTask
+## ON05NS23 omitted 23 in ART
+## EN06IR08 misslabeled ER06IR08 in BoxTask
+## EN04IR22 noncapetilized EN04ir22 in BoxTask
+## en08ar12 misslabeled as en09ar12 in BeadsTask
+## kanhil labeled kanhil_misforstod in BoxTask
+## STERIG decapitalized as sterig in ART
+
+# Missing ID's matched by timestamps, sequence, and computer ID.
+## 1 (Box) is EN3HN11
+## *blank* (Beads) is en05ar27 - Fixed during extraction
+
+# Clearly test-runs by experimenters based on time-stamps and/or names.
+## 56yg (ART) - Removed in ART-extraction
+## ghgj (ART) - Removed in ART-extraction
+## 43gref (Beads) - Removed in Beads-extraction
+## test (multiple) - Removed
+## fdsfdsfds (ART) - Removed in ART-extraction
+## htr645 (ART) - Removed in ART-extraction
+## 432few (Beads) - Removed in Beads-extraction
+## 543543 (Beads) - Removed in Beads-extraction
+## er34er34 (Beads & Box) - Removed
+## hjkhjk (Beads) - Removed in Beads-extraction
