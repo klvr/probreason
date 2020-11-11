1a61eee6c8f90665c72ffcdf1a88141199b0daad
Kristoffer Klevjer <kristoffer@klevjer.no>
2020-11-11 22:37:50 +0100

    All manual changes to ID's documented

--- Manual-ID-fix.R
+++ Manual-ID-fix.R
@@ -9,6 +9,24 @@
 ## en08ar12 misslabeled as en09ar12 in BeadsTask - Fixed in Data-extraction-BeadsTwojarTask.R
 ## kanhil labeled kanhil_misforstod in BoxTask - Duplicate removed in Data-extraction-BoxNormXlsx.R
 ## STERIG decapitalized as sterig in ART - Fixed in hBayesDM-ART.R
+## NE05EL22 misslabeled as NE05EL20 in Box - Fixed in Data-extraction-BoxNormXlsx.R
+## MA06HA94 decapitalized as ma06ha94 in BoxTask - Fixed in Data-extraction-BoxNormXlsx.R
+## kroåse creates a lot of trouble by including a Norwegian letter, changed in all tasks (beads, box, ART and CAPE) to kroase
+## CAPE fixes (all in Data-extraction-Cape.R):
+### AD03IM02 -> ad03im02
+### au06ne01 -> AU06NE01
+### ELEN01NT29 -> ELEN1NT29
+### EN05AR27 -> en05ar27
+### EN08AR12 -> en08ar12
+### Lavern -> lavern
+### ND03IR04 -> nd03ir04
+### ON06LF03 -> on06lf03
+### sterig -> STERIG
+###	ENAR25 -> EN05AR25
+### EN07AS30 -> EN06AS30
+### EN06NE21 -> EN6NE21
+### rd06ik27 -> id06ik27
+### SØ05LD08-> IE05LD08
 
 # Missing ID's matched by timestamps, sequence, and computer ID.
 ## 1 (Box) is EN3HN11 - Fixed in Data-extraction-BoxNormXlsx.R
