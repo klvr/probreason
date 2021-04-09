a833e43bf45562326159fd459061909ff38fca08
Kristoffer Klevjer <kristoffer@klevjer.no>
2021-04-09 16:45:07 +0200

    Fixed spelling - Added missing sequence - Added free sequence recoding

--- Ideal Baysian Observer.R
+++ Ideal Baysian Observer.R
@@ -1,5 +1,6 @@
 # Ideale Baysian Observer
-## Missing: 2018 extracted sequences and IBO for those
+# Run after masterscript/merging is done
+# Run before IBO Free Sequence (relient on data, and variables from here)
 
 # 2018
 ## One Jar
@@ -7,7 +8,7 @@
 ## Tone
 ### Seq1_8515, Seq3_8515, Seq1_6040, Seq3_6040
 ## Two Jars
-### Seq1_8515, Seq3_8515, Seq1_6040, Seq3_6040
+### Seq1_8515, Seq3_8515, Seq1_6040, Seq3_6040, Seq4_8515
 ## Box Irreg
 ### Free sequence (extracted)
 ## Box Norm
@@ -18,7 +19,7 @@
 ## Box Norm
 ### colour_sequences_12 (3 seqs), colour_sequences_6or9_a (3 seqs), colour_sequences_6or9_b (3 seqs)
 
-# Actual sequences Beads tasks
+# Actual sequences Beads-tasks
 Seq1_8515 <- c(rep(1, 9), 0, rep(1,10))
 Seq2_8515 <- c(rep(1, 16), 0, rep(1, 3))
 Seq3_8515 <- c(1, 0, rep(1, 5), 0, rep(1, 3), 0, rep(1, 5), 0, 1, 1)
@@ -27,7 +28,7 @@ Seq3_6040 <- c(0, rep(1, 3), 0, 1, 0, 1, 1, 0, 0, 1, 0, rep(1, 4), 0, 1, 0)
 Seq4_8515 <- c(rep(1,7),0,rep(1,12))
 AllBeadsSeq <- c("Seq1_8515", "Seq2_8515", "Seq3_8515","Seq4_8515", "Seq1_6040", "Seq3_6040")
 
-# Actual sequences Box task (2020 fixed)
+# Actual sequences Box-task (2020)
 Seq_12_1 <- c(1,1,1,0,1,1,1,1,0,1,0,1) #0: Blue, 1: Red
 Seq_12_2 <- c(0,1,0,1,0,0,1,0,1,0,1,0) #0: Yellow, 1: Green
 Seq_12_3 <- c(0,0,1,0,1,0,1,0,0,1,0,0) #0: Purple, 1: White
@@ -55,7 +56,7 @@ ThresholdBeads <- cbind(ThresholdBeads, Threshold)
 }
 colnames(ThresholdBeads) <- AllBeadsSeq
 
-# Box
+# Box-fixed sequence
 ## Combinatorics, yields a number between 0 and 1, where 1 is IO can make a certain choice
 ThresholdBox12 <- as.numeric()
 for (i in AllBoxSeqUnlimit) {
@@ -97,3 +98,50 @@ ThresholdBox6 <- cbind(ThresholdBox6, ThresholdSeq)
 }
 colnames(ThresholdBox6) <- AllBoxSeqLimit6
 
+#Box free sequence recoding
+## Irreg
+### Trial1
+sequence <- c(0,0,0,1,0,0,0,0,0,0,1,0,0,0,1)
+Trial <- rep(1,15)
+IrregCol1 <- paste(rep("box_",15),seq(1,15,1),sep="")
+IrregCol1 <- cbind(IrregCol1,sequence, Trial)
+### Trial2
+sequence <- c(1,1,1,1,1,0,1,1,1,0,1,1,0,1,1)
+Trial <- rep(2,15)
+IrregCol2 <- paste(rep("box_",15),seq(60,74,1),sep="")
+IrregCol2 <- cbind(IrregCol2, sequence, Trial)
+### Trial3
+sequence <- c(1,0,1,1,0,1,0,1,0,1,1,0,0,1,1)
+Trial <- rep(3,15)
+IrregCol3 <- paste(rep("box_",15),seq(75,89,1),sep="")
+IrregCol3 <- cbind(IrregCol3,sequence, Trial)
+### Trial4
+sequence <- c(1,0,0,1,0,0,1,0,0,0,1,0,0,0,1)
+Trial <- rep(4,15)
+IrregCol4 <- paste(rep("box_",15),seq(90,104,1),sep="")
+IrregCol4 <- cbind(IrregCol4, sequence, Trial)
+### All
+IrregCol <- rbind(IrregCol1,IrregCol2,IrregCol3,IrregCol4)
+## Norm
+### Trial1
+sequence <- c(0,0,0,1,0,0,0,0,0,0,1,0,0,0,1)
+NormCol1 <- paste(rep("box_",15),seq(1,15,1),sep="")
+NormCol1 <- cbind(NormCol1, sequence)
+### Trial2
+sequence <- c(1,1,1,1,1,0,1,1,1,0,1,1,0,1,1)
+NormCol2 <- c("box",paste(rep("box_",14),seq(16,29,1),sep=""))
+NormCol2 <- cbind(NormCol2, sequence)
+### Trial3
+sequence <- c(1,0,1,1,0,1,0,1,0,1,1,0,0,1,1)
+NormCol3 <- paste(rep("box_",15),seq(30,44,1),sep="")
+NormCol3 <- cbind(NormCol3, sequence)
+### Trial4
+sequence <- c(1,0,0,1,0,0,1,0,0,0,1,0,0,0,1)
+NormCol4 <- paste(rep("box_",15),seq(45,59,1),sep="")
+NormCol4 <- cbind(NormCol4, sequence)
+### All
+NormCol <- rbind(NormCol1, NormCol2, NormCol3, NormCol4)
+
+rm(list = c("AllBeadsSeq", "AllBoxSeqLimit6", "AllBoxSeqLimit9", "AllBoxSeqUnlimit", "high", "i", "lengde", "low", "probh", "probhigh", "probl", "problow", "seq", "Seq_12_1", "Seq_12_2", "Seq_12_3", "Seq_6or9a_1","Seq_6or9a_2","Seq_6or9a_3","Seq_6or9b_1","Seq_6or9b_2","Seq_6or9b_3", "sequence", "Threshold", "ThresholdSeq", "Seq1_6040", "Seq1_8515", "Seq2_8515", "Seq3_6040", "Seq3_8515", "Seq4_8515", "IrregCol1","IrregCol2","IrregCol3","IrregCol4","NormCol1","NormCol2","NormCol3","NormCol4"))
+  
+  
