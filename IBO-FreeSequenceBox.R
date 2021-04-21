ff0aefa2322b6f9b1e647214363f563462a9e3d2
Kristoffer Klevjer <kristoffer@klevjer.no>
2021-04-21 15:48:40 +0200

    Correct extraction for 2018 NormBox IBO added. Currently a bit messy.

--- IBO-FreeSequenceBox.R
+++ IBO-FreeSequenceBox.R
@@ -61,26 +61,140 @@ IBO[!is.na(IBO[,14]),15] <- BoxIrregIBOCol[,10]
 IBO[!is.na(IBO[,14]),16] <- BoxIrregIBOCol[,11]
 IBO[!is.na(IBO[,14]),17] <- BoxIrregIBOCol[,12]
 
+##Load all potential sequences for 8515
+Seq8515 <- read.csv('0.85_0.15_Sequences.csv')
+Seq8515 <- Seq8515[,-1]
+colnames(Seq8515) <- NULL
+for (i in 1:ncol(Seq8515)) {
+  Seq8515[,i] <- as.character(Seq8515[,i])
+  Seq8515[,i] <- replace(Seq8515[,i], Seq8515[,i]=="True", 1)
+  Seq8515[,i] <- replace(Seq8515[,i], Seq8515[,i]=="False", 0)
+  Seq8515[,i] <- as.numeric(Seq8515[,i])
+}
+
+##Load all potential sequences for 6040
+Seq6040 <- read.csv('0.6_0.4_Sequences.csv')
+Seq6040 <- Seq6040[,-1]
+colnames(Seq6040) <- NULL
+for (i in 1:ncol(Seq6040)) {
+  Seq6040[,i] <- as.character(Seq6040[,i])
+  Seq6040[,i] <- replace(Seq6040[,i], Seq6040[,i]=="True", 1)
+  Seq6040[,i] <- replace(Seq6040[,i], Seq6040[,i]=="False", 0)
+  Seq6040[,i] <- as.numeric(Seq6040[,i])
+}
+
 #2018
 ## Norm
 IBO <- merge(IBO, BoxTaskSeen, by = "row.names", all.x = TRUE, all.y = FALSE)
 row.names(IBO) <- IBO[,1]
 IBO <- IBO[,-1]
 for (i in 1:ncol(IBO)) {IBO[,i] <- as.numeric(as.character(IBO[,i]))}
+
 for (i in 1:nrow(IBO)) {
-  if(!is.na(IBO[i,18])) {
-    IBO[i,18] <- (dbinom(as.numeric(IBO[i,32]), as.numeric(IBO[i,18]), prob = 0.85) / (dbinom(as.numeric(IBO[i,32]), as.numeric(IBO[i,18]), prob = 0.85) + dbinom(as.numeric(IBO[i,32]), as.numeric(IBO[i,18]), prob = 0.15)) )
+  if(is.na(IBO[i,18]) + is.na(IBO[i,32]) == 0){
+    if(IBO[i,32] == 0){Seq2 <- NULL} else {Seq2 <- rep(1, times = IBO[i,32])}
+    if(IBO[i,18] == 0){Seq1 <- NULL} else {Seq1 <- rep(0, times = (IBO[i,18]-length(Seq2)))}
+    ExSeq <- c(Seq1,Seq2)
+    ExSeqR <- (ExSeq*-1)+1
+    ExSeq <- as.data.frame(rbind(ExSeq))
+    ExSeqR <- as.data.frame(rbind(ExSeqR))
+    RedSeq <- Seq8515[,1:(length(ExSeq))]
+    Stop1 <- mean(Seq8515[,1])
+    if (IBO[i,32]==3){IBO[i,18] <- 1} else {
+      if (ncol(ExSeq)==1) {IBO[i,18] <- Stop1} else {
+        sum <- NULL
+        sumR <- NULL
+        for (x in 1:nrow(Seq8515)) {
+          tf <- rowSums(ExSeq == RedSeq[x,])==length(ExSeq)
+          sum <- c(sum,tf)
+          tfR <- rowSums(ExSeqR == RedSeq[x,])==length(ExSeqR)
+          sumR <- c(sumR, tfR)
+        }
+        sum <- sum(sum)
+        sumR <- sum(sumR)
+        if (sum > sumR) {IBO[i,18] <- sum/(sum+sumR)} else {IBO[i,18] <- sumR/(sum+sumR)}
+      }
+    }
   }
-  if(!is.na(IBO[i,19])) {
-    IBO[i,19] <- (dbinom(as.numeric(IBO[i,33]), as.numeric(IBO[i,19]), prob = 0.85) / (dbinom(as.numeric(IBO[i,33]), as.numeric(IBO[i,19]), prob = 0.85) + dbinom(as.numeric(IBO[i,33]), as.numeric(IBO[i,19]), prob = 0.15)) )
+  if(is.na(IBO[i,19]) + is.na(IBO[i,33]) == 0){
+    if(IBO[i,33] == 0){Seq2 <- NULL} else {Seq2 <- rep(1, times = IBO[i,33])}
+    if(IBO[i,19] == IBO[i,33]){Seq1 <- NULL} else {Seq1 <- rep(0, times = (IBO[i,19]-length(Seq2)))}
+    ExSeq <- c(Seq1,Seq2)
+    ExSeqR <- (ExSeq*-1)+1
+    ExSeq <- as.data.frame(rbind(ExSeq))
+    ExSeqR <- as.data.frame(rbind(ExSeqR))
+    RedSeq <- Seq8515[,1:(length(ExSeq))]
+    Stop1 <- mean(Seq8515[,1])
+    if (length(Seq1)==3){IBO[i,19] <- 1} else {
+      if (ncol(ExSeq)==1) {IBO[i,19] <- Stop1} else {
+        sum <- NULL
+        sumR <- NULL
+        for (x in 1:nrow(Seq8515)) {
+          tf <- rowSums(ExSeq == RedSeq[x,])==length(ExSeq)
+          sum <- c(sum,tf)
+          tfR <- rowSums(ExSeqR == RedSeq[x,])==length(ExSeqR)
+          sumR <- c(sumR, tfR)
+        }
+        sum <- sum(sum)
+        sumR <- sum(sumR)
+        if (sum > sumR) {IBO[i,19] <- sum/(sum+sumR)} else {IBO[i,19] <- sumR/(sum+sumR)}
+      }
+    }
   }
-  if(!is.na(IBO[i,20])) {
-    IBO[i,20] <- (dbinom(as.numeric(IBO[i,34]), as.numeric(IBO[i,20]), prob = 0.60) / (dbinom(as.numeric(IBO[i,34]), as.numeric(IBO[i,20]), prob = 0.60) + dbinom(as.numeric(IBO[i,34]), as.numeric(IBO[i,20]), prob = 0.40)) )
+  if(is.na(IBO[i,20]) + is.na(IBO[i,34]) == 0){
+    if(IBO[i,34] == 0){Seq2 <- NULL} else {Seq2 <- rep(1, times = IBO[i,34])}
+    if(IBO[i,20] == 0){Seq1 <- NULL} else {Seq1 <- rep(0, times = (IBO[i,20]-length(Seq2)))}
+    ExSeq <- c(Seq1,Seq2)
+    ExSeqR <- (ExSeq*-1)+1
+    ExSeq <- as.data.frame(rbind(ExSeq))
+    ExSeqR <- as.data.frame(rbind(ExSeqR))
+    RedSeq <- Seq6040[,1:(length(ExSeq))]
+    Stop1 <- mean(Seq6040[,1])
+    if (IBO[i,34]==9){IBO[i,20] <- 1} else {
+      if (ncol(ExSeq)==1) {IBO[i,20] <- Stop1} else {
+        sum <- NULL
+        sumR <- NULL
+        for (x in 1:nrow(Seq6040)) {
+          tf <- rowSums(ExSeq == RedSeq[x,])==length(ExSeq)
+          sum <- c(sum,tf)
+          tfR <- rowSums(ExSeqR == RedSeq[x,])==length(ExSeqR)
+          sumR <- c(sumR, tfR)
+        }
+        sum <- sum(sum)
+        sumR <- sum(sumR)
+        if (sum > sumR) {IBO[i,20] <- sum/(sum+sumR)} else {IBO[i,20] <- sumR/(sum+sumR)}
+      }
+    }
   }
-  if(!is.na(IBO[i,21])) {
-    IBO[i,21] <- (dbinom(as.numeric(IBO[i,35]), as.numeric(IBO[i,21]), prob = 0.60) / (dbinom(as.numeric(IBO[i,35]), as.numeric(IBO[i,21]), prob = 0.60) + dbinom(as.numeric(IBO[i,35]), as.numeric(IBO[i,21]), prob = 0.40)) )
+  if(is.na(IBO[i,21]) + is.na(IBO[i,35]) == 0){
+    if(IBO[i,35] == 0){Seq2 <- NULL} else {Seq2 <- rep(1, times = IBO[i,35])}
+    if(IBO[i,21] == IBO[i,35]){Seq1 <- NULL} else {Seq1 <- rep(0, times = (IBO[i,21]-length(Seq2)))}
+    ExSeq <- c(Seq1,Seq2)
+    ExSeqR <- (ExSeq*-1)+1
+    ExSeq <- as.data.frame(rbind(ExSeq))
+    ExSeqR <- as.data.frame(rbind(ExSeqR))
+    RedSeq <- Seq6040[,1:(length(ExSeq))]
+    Stop1 <- mean(Seq6040[,1])
+    if (length(Seq1)==10){IBO[i,21] <- 1} else {
+      if (ncol(ExSeq)==1) {IBO[i,21] <- Stop1} else {
+        sum <- NULL
+        sumR <- NULL
+        for (x in 1:nrow(Seq6040)) {
+          tf <- rowSums(ExSeq == RedSeq[x,])==length(ExSeq)
+          sum <- c(sum,tf)
+          tfR <- rowSums(ExSeqR == RedSeq[x,])==length(ExSeqR)
+          sumR <- c(sumR, tfR)
+        }
+        sum <- sum(sum)
+        sumR <- sum(sumR)
+        if (sum > sumR) {IBO[i,21] <- sum/(sum+sumR)} else {IBO[i,21] <- sumR/(sum+sumR)}
+      }
+    }
   }
 }
+
+##Irreg
+
 
 #Removing redundent data
 IBO <- IBO[,-c(22,32,33,34,35)]
