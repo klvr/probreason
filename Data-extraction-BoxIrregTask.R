ebfad1d850722f6dcf589fcbc0942683122bcc80
Kristoffer Klevjer <kristoffer@klevjer.no>
2021-04-23 13:59:55 +0200

    Only unique box-presses recorded

--- Data-extraction-BoxIrregTask.R
+++ Data-extraction-BoxIrregTask.R
@@ -4,7 +4,6 @@ BoxIrregDtD <- row.names(c("ID", "DtD1", "DtD2", "DtD3", "DtD4"))
 BoxIrregDtD <- as.data.frame(BoxIrregDtD)
 
 for(i in BoxIrregTaskPath) {
-  print(i)
   IrrData <- read.delim(i)
   IrrData$EXP <- as.character(IrrData$EXP)
   colnames(IrrData) <- c("time","EXP" ,"press")
@@ -29,6 +28,7 @@ for(i in BoxIrregTaskPath) {
   boxchoice1 <- boxchoice1[seq(from = 1, to = length(boxchoice1), by = 2)]
   pressed1 <- as.character(IrrData$press[as.numeric(keypressed[2])])
   Trial1 <- cbind(boxchoice1, pressed1)
+  Trial1 <- unique(Trial1)
   boxchoice2 <- IrrData[as.numeric(keypressed[2]):as.numeric(keypressed[3]),]
   boxchoice2 <- boxchoice2[grepl(color, boxchoice2$press),]
   boxchoice2 <- strsplit(as.character(unique(boxchoice2$press)), split = ":")
@@ -36,6 +36,7 @@ for(i in BoxIrregTaskPath) {
   boxchoice2 <- boxchoice2[seq(from = 1, to = length(boxchoice2), by = 2)]
   pressed2 <- as.character(IrrData$press[as.numeric(keypressed[3])])
   Trial2 <- cbind(boxchoice2, pressed2)
+  Trial2 <- unique(Trial2)
   boxchoice3 <- IrrData[as.numeric(keypressed[3]):as.numeric(keypressed[4]),]
   boxchoice3 <- boxchoice3[grepl(color, boxchoice3$press),]
   boxchoice3 <- strsplit(as.character(unique(boxchoice3$press)), split = ":")
@@ -43,6 +44,7 @@ for(i in BoxIrregTaskPath) {
   boxchoice3 <- boxchoice3[seq(from = 1, to = length(boxchoice3), by = 2)]
   pressed3 <- as.character(IrrData$press[as.numeric(keypressed[4])])
   Trial3 <- cbind(boxchoice3, pressed3)
+  Trial3 <- unique(Trial3)
   boxchoice4 <- IrrData[as.numeric(keypressed[4]):as.numeric(keypressed[5]),]
   boxchoice4 <- boxchoice4[grepl(color, boxchoice4$press),]
   boxchoice4 <- strsplit(as.character(unique(boxchoice4$press)), split = ":")
@@ -50,6 +52,7 @@ for(i in BoxIrregTaskPath) {
   boxchoice4 <- boxchoice4[seq(from = 1, to = length(boxchoice4), by = 2)]
   pressed4 <- as.character(IrrData$press[as.numeric(keypressed[5])])
   Trial4 <- cbind(boxchoice4, pressed4)
+  Trial4 <- unique(Trial4)
   id <- strsplit(i, split ="_boxtaskirreg/")[1]
   id <- unlist(id)[1]
   id <- strsplit(id, split="")
